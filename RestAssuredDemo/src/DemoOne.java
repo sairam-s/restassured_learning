927d6bbe470590d6d45b6d0df0df50c23017df6c
Sairam Sivaswami <sairams@orionhealth.com>
2018-04-06 09:08:07 +1200

    Updated the test to use junit to run, and completed heade, body and general asserstions for a GET request.
    issue:
    reviewer:

--- RestAssuredDemo/src/DemoOne.java
+++ RestAssuredDemo/src/DemoOne.java
@@ -1,13 +1,15 @@
-import io.restassured.RestAssured;
+import org.junit.Test;
 
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class DemoOne {
 
-	public static void main(String[] args) {
-		
+@Test
+public void Test(){
+   
    RestAssured.baseURI = "https://maps.googleapis.com";
    given().
     param("location","-33.8670522,151.1957362").
@@ -17,7 +19,8 @@ public class DemoOne {
      get("/maps/api/place/nearbysearch/json").
     then().assertThat().
      statusCode(200).and().contentType(ContentType.JSON).and().
-     body("results[0].name", equalTo("Sydney"));
+     body("results[0].name", equalTo("Sydney")).and().body("results[0].place_id", equalTo("ChIJP3Sa8ziYEmsRUKgyFmh9AQM")).
+     and().header("Server", "pablo");
      
 
 	}
